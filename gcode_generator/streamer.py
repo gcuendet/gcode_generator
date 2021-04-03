68898c36ad059b25fec79b4f41a38a12b9a86240
Gabriel Cuendet <gabriel.cuendet@gmail.com>
2021-04-03 07:30:41 +0200

    Move the pen up at the end of the streaming

--- gcode_generator/streamer.py
+++ gcode_generator/streamer.py
@@ -4,13 +4,19 @@ import requests
 def post_gcode(gcode: str, url: str, max_load_kB: float = 1):
     payload = b""
     payload_size = 0
-    for l in gcode.splitlines(keepends=True):
-        new_line = l.encode()
-        if (payload_size + len(new_line)) < max_load_kB * 1e3:
-            payload += new_line
-            payload_size += len(new_line)
-        else:
-            print("POST\n{}".format(payload.decode()))
-            r = requests.post(url, payload)
-            payload = new_line
-            payload_size = len(new_line)
+    try:
+        for l in gcode.splitlines(keepends=True):
+            new_line = l.encode()
+            if (payload_size + len(new_line)) < max_load_kB * 1e3:
+                payload += new_line
+                payload_size += len(new_line)
+            else:
+                print("POST\n{}".format(payload.decode()))
+                r = requests.post(url, payload, timeout=30)
+                payload = new_line
+                payload_size = len(new_line)
+    finally:
+        rst_msg = ";Pen Up\nM300 S135\nG4 P120\nG1 X0.0 Y0.0 F775.0"
+        payload = rst_msg.encode()
+        print("POST\n{}".format(payload.decode()))
+        r = requests.post(url, payload, timeout=5)
